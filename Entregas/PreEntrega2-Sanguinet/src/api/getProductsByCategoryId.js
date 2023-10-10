6890199ff33fb9d0f3f158af40979b3003a26651
Matias Sanguinet <rmsrcel@gmail.com>
2023-10-10 19:18:32 -0300

    se agrega llamado a api a fuego

--- Entregas/PreEntrega2-Sanguinet/src/api/getProductsByCategoryId.js
+++ Entregas/PreEntrega2-Sanguinet/src/api/getProductsByCategoryId.js
@@ -1,5 +1,7 @@
-const baseUrl = 'https://api.escuelajs.co/api/v1/products/?categoryId='; // import.meta.env.VITE_API_URL + import.meta.env.VITE_API_FILTER_CATEGORY_ID;
+const baseUrl = 'https://api.escuelajs.co/api/v1/products/?categoryId='; //  import.meta.env.VITE_API_URL + import.meta.env.VITE_API_FILTER_CATEGORY_ID;
 //url base + url cateogira
+
+//https://api.escuelajs.co/api/v1/products/?categoryId='
 export const getProductsByCategoryId = async (categoryId) => {
   let url = baseUrl + categoryId;
   try {
