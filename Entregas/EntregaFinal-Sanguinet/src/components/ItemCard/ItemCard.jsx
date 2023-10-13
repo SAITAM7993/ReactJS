f6d381e93553f51bf11e1f345e17b82a7f860c5b
Matias Sanguinet <rmsrcel@gmail.com>
2023-10-13 10:35:33 -0300

    se cambia link de card a img en vez de card entera

--- Entregas/EntregaFinal-Sanguinet/src/components/ItemCard/ItemCard.jsx
+++ Entregas/EntregaFinal-Sanguinet/src/components/ItemCard/ItemCard.jsx
@@ -17,15 +17,15 @@ const ProductCard = ({
   stock,
 }) => {
   return (
-    <Link
-      key={`product-${id}`}
-      to={`/item/${id}`}
+    <Card
+      className='card__custom'
+      sx={{
+        height: '100%',
+      }}
     >
-      <Card
-        className='card__custom'
-        sx={{
-          height: '100%',
-        }}
+      <Link
+        key={`product-${id}`}
+        to={`/item/${id}`}
       >
         <CardMedia
           className='card__Img'
@@ -34,62 +34,64 @@ const ProductCard = ({
           height='auto'
           image={image}
         />
-        <CardContent className='card__body'>
-          <Typography
-            gutterBottom
-            variant='body1'
-            component='p'
-          >
-            {title}
-          </Typography>
-          <Typography
-            gutterBottom
-            variant='body2'
-            component='p'
-            color='text.secondary'
-          >
-            {category}
-          </Typography>
-          <Typography
-            my={1}
-            sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              display: '-webkit-box',
-              WebkitLineClamp: '2',
-              WebkitBoxOrient: 'vertical',
-            }}
-            variant='body2'
-            color='text.secondary'
-          >
-            {description}
-          </Typography>
-          <Typography
-            my={1}
-            variant='body2'
-            component='p'
-          >
-            STOCK: {stock} | ID: {id}
-          </Typography>
+      </Link>
+      <CardContent className='card__body'>
+        <Typography
+          gutterBottom
+          variant='body1'
+          component='p'
+        >
+          {title}
+        </Typography>
 
-          <Typography
-            color='secondary'
-            mt={1}
-            variant='h5'
-            component='div'
-          >
-            ${price}
-          </Typography>
-        </CardContent>
-        <CardActions className='card__actions'>
-          <ItemActions
-            initial={1}
-            stock={stock}
-            onAdd={(quantity) => console.log('cantidad agregada ', quantity)}
-          />
-        </CardActions>
-      </Card>
-    </Link>
+        <Typography
+          gutterBottom
+          variant='body2'
+          component='p'
+          color='text.secondary'
+        >
+          {category}
+        </Typography>
+        <Typography
+          my={1}
+          sx={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: '2',
+            WebkitBoxOrient: 'vertical',
+          }}
+          variant='body2'
+          color='text.secondary'
+        >
+          {description}
+        </Typography>
+        <Typography
+          my={1}
+          variant='body2'
+          component='p'
+        >
+          STOCK: {stock} | ID: {id}
+        </Typography>
+
+        <Typography
+          color='secondary'
+          mt={1}
+          variant='h5'
+          component='div'
+        >
+          ${price}
+        </Typography>
+      </CardContent>
+
+      <CardActions className='card__actions'>
+        <ItemActions
+          initial={1}
+          stock={stock}
+          onAdd={(quantity) => console.log('cantidad agregada ', quantity)}
+        />
+      </CardActions>
+    </Card>
   );
 };
 export default ProductCard;
